d4e51f10cddbd5c60aec77cd1f24df45a48db9cf
Daniel Paul Tchatchouang Ouokam <tchatchouang.ouokam@gmail.com>
2021-05-15 06:45:10 +0200

    putting all the files in src folder

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+
+/* MOBILE NAV OPENER */
+document.querySelector('.menu-toggle').addEventListener('click', function(){ document.querySelector('.mobile-nav').classList.toggle('visible'); });
+
+
+/* LOADER */
+window.addEventListener('load',function(){ document.querySelector('.loader').className += " hidden"; });
+
+
+
